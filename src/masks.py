eaecd6819ba7539c5cf5e6336285b07b5c229523
Stepan Baidakov <stepanbaidakov@icloud.com>
2025-01-30 21:30:01 +0100

    fixes on widget.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -10,9 +10,3 @@ def get_mask_account(account_number: int) -> str:
     account_number_str = str(account_number)
     masked_number = f"**{account_number_str[-4:]}"
     return masked_number
-
-
-masked_card_number = get_mask_card_number(input("Введите номер карты"))
-print(masked_card_number)
-masked_account = get_mask_account(input("Введите номер cчета"))
-print(masked_account)
--- src/widget.py
+++ src/widget.py
@@ -1,56 +1,48 @@
-# def mask_account_card(client_info: str) -> str:
-#     """Обрабатывает информацию о картах и счетах"""
-#     card_type = ""
-#     if "Счет" in client_info:
-#         for letter in client_info:
-#             if letter.isalpha() or letter == " ":
-#                 card_type += letter
-#         nums_list = []
-#         for num in client_info:
-#             if num.isdigit():
-#                 nums_list.append(num)
-#         card_number = "".join(nums_list)
-#         masked_account_number = f"**{card_number[-4:]}"
-#         masked_client_info = f"{card_type} {masked_account_number}"
-#         return masked_client_info
-#     else:
-#         for letter in client_info:
-#             if letter.isalpha() or letter == " ":
-#                 card_type += letter
-#     nums_list = []
-#     for num in client_info:
-#         if num.isdigit():
-#             nums_list.append(num)
-#     card_number = "".join(nums_list)
-#     masked_card_number= f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-#     masked_client_info = f"{card_type} {masked_card_number}"
-#     return masked_client_info
-# print(mask_account_card("Visa Classic 6831982476737658"))
+from src.masks import get_mask_account, get_mask_card_number
 
 
-# def get_date(date):
-#      """Орабатывает дату"""
-#      correct_date = f"{date[8:10]}.{date[5:7]}.{date[:4]}"
-#      return correct_date
-# print(get_date("2024-03-11ljasljflajlfjla"))
-
-
-
-def masked_account_card(client_info: str):
+def mask_account_card(client_info: str) -> str:
+    """Обрабатывает информацию о картах и счетах"""
     splited = client_info.split(" ")
     words = []
     for part in splited:
         if part.isdigit():
             if "Счет" in splited:
-                masked_account_number = f"**{part[-4:]}"
-                masked_number = masked_account_number
+                    masked_number = get_mask_account(part)
             else:
-                masked_card_number= f"{part[:4]} {part[4:6]}** **** {part[-4:]}"
-                masked_number = masked_card_number
+                    masked_number = get_mask_card_number(part)
         if part.isalpha():
-            words.append(part)
+                words.append(part)
     card_type = " ".join(words)
     masked_client_info = f"{card_type} {masked_number}"
     return masked_client_info
 
-print(masked_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Счет 73654108430135874305"))
+
+def get_date(date):
+     """Орабатывает дату"""
+     correct_date = f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+     return correct_date
+
+print(get_date("2023-12-11ljasljflajlfjla"))
+
+
+
+# def masked_account_card(client_info: str):
+#     splited = client_info.split(" ")
+#     words = []
+#     for part in splited:
+#         if part.isdigit():
+#             if "Счет" in splited:
+#                 masked_account_number = f"**{part[-4:]}"
+#                 masked_number = masked_account_number
+#             else:
+#                 masked_card_number= f"{part[:4]} {part[4:6]}** **** {part[-4:]}"
+#                 masked_number = masked_card_number
+#         if part.isalpha():
+#             words.append(part)
+#     card_type = " ".join(words)
+#     masked_client_info = f"{card_type} {masked_number}"
+#     return masked_client_info
+#
+# print(masked_account_card("Счет 35383033474447895560"))
